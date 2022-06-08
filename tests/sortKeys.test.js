6ca5739e6cfeb4f5d99f9f2104138cba900fec03
Heydon <heydon@heydonworks.com>
2022-06-08 11:42:38 +0100

    jest with native esm

---
+++ tests/sortKeys.test.js
@@ -0,0 +1,14 @@
+'use strict';
+import { sortKeys } from "../utils/sortKeys";
+
+test('Sorts an object\'s keys alphabetically', () => {
+  const object = {
+    b: true,
+    c: true,
+    z: true,
+    a: true
+  }
+
+  const sorted = sortKeys(object);
+  expect(Object.keys(sorted)[0]).toBe('a');
+});
